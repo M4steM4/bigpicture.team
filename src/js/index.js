8a69143b18db7f5815359f6746b4294385a870c7
M4steM4 <m4stem4@naver.com>
2017-09-06 17:07:21 +0900

    Add file

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 if (module.hot) {
-    console.log("test ");
-    module.hot.accept();
+  module.hot.accept();
 }
 
 import './index.scss';
@@ -8,114 +7,115 @@ import './index.scss';
 
 $(document).ready(function() {
 
-
-  var background = {}
-    background.initializr = function (){
+  function backgroundEvent() {
+    var background = {}
+    background.initializr = function () {
       var $this = this;
-      //option
-      $this.id = "background_css3";
-      $this.style = {bubbles_color:"#ceff54",stroke_width:0, stroke_color :"black"};
-      $this.bubbles_number = 30;
-      $this.speed = [1500,8000]; //milliseconds
+      // Option
+      $this.id = "#background"; // ID
+      $this.style = { bubbles_color:"#911" , stroke_width:0 }; // Color
+      $this.bubbles_number = 10; // Count
+      $this.speed = [2000, 8000]; //milliseconds
       $this.max_bubbles_height = $this.height;
       $this.shape = false // 1 : circle | 2 : triangle | 3 : rect | false :random
-      if($("#"+$this.id).lenght > 0){
+      if ($($this.id).lenght > 0) {
         $("#"+$this.id).remove();
       }
-      $this.object = $("<div style='z-inde:-1;margin:0;padding:0; overflow:hidden;position:fixed;bottom:0' id='"+$this.id+"'> </div>'").appendTo("body");
-
-      $this.ww = $(window).width()
-      $this.wh = $(window).height()
+      // Setting
+      $this.object = $("<div style='z-index:-1; margin:0; padding:0; overflow:hidden; position:fixed;bottom:0' id='"+$this.id+"'> </div>'").appendTo("body");
+      $this.ww = $(window).width();
+      $this.wh = $(window).height();
       $this.width = $this.object.width($this.ww);
       $this.height = $this.object.height($this.wh);
-      $("body").prepend("<style>.shape_background {transform-origin:center; width:80px; height:80px; background: "+$this.style.bubbles_color+"; position: absolute}</style>");
 
       for (i = 0; i < $this.bubbles_number; i++) {
-          $this.generate_bubbles()
+        $this.generate_bubbles();
       }
     }
+    // Create function
+    background.generate_bubbles = function() {
+      var $this = this;
+      var base = $("<div class='star'>&#9733;</div>");
+      var bolla = base.css({"font-size": "50px"});
+      var rn_size = $this.ran(.6,1.2);
 
-     background.generate_bubbles = function() {
-       var $this = this;
-       var base = $("<div class='shape_background'></div>");
-       var shape_type = $this.shape ? $this.shape : Math.floor($this.rn(1,3));
-       if(shape_type == 1) {
-         var bolla = base.css({borderRadius: "50%"})
-       }else if (shape_type == 2){
-         var bolla = base.css({width:0, height:0, "border-style":"solid","border-width":"0 40px 69.3px 40px","border-color":"transparent transparent "+$this.style.bubbles_color+" transparent", background:"transparent"});
-       }else{
-         var bolla = base;
-       }
-       var rn_size = $this.rn(.8,1.2);
-       bolla.css({"transform":"scale("+rn_size+") rotate("+$this.rn(-360,360)+"deg)", top:$this.wh+100, left:$this.rn(-60, $this.ww+60)});
-       bolla.appendTo($this.object);
-       bolla.transit({top: $this.rn($this.wh/2,$this.wh/5), "transform":"scale("+rn_size+") rotate("+$this.rn(-360,360)+"deg)", opacity: 0},$this.rn($this.speed[0],$this.speed[1]), function(){
-         $(this).remove();
-         $this.generate_bubbles();
-       })
-
-      }
-
-
-  background.rn = function(from, to, arr) {
-    if(arr){
-            return Math.random() * (to - from + 1) + from;
-    }else{
-      return Math.floor(Math.random() * (to - from + 1) + from);
+      bolla.css({"transform":"scale("+rn_size+") rotate("+$this.ran(-360,360)+"deg)", top:$this.wh+100, left:$this.ran(-60, $this.ww+60)});
+      bolla.appendTo($this.object);
+      bolla.transit({top: $this.ran($this.wh/2, $this.wh/10), "transform":"scale("+rn_size+") rotate("+$this.ran(-360,360)+"deg)", opacity: 0},$this.ran($this.speed[0], $this.speed[1]), function() {
+        $(this).remove();
+        $this.generate_bubbles();
+      });
     }
+    // Random fucntion
+    background.ran = function(from, to, arr) {
+      if(arr) {
+        return Math.random() * (to - from + 1) + from;
+      } else {
+        return Math.floor(Math.random() * (to - from + 1) + from);
       }
-  background.initializr();
-
+    }
+    background.initializr();
+  }
 
 
   var code = "";
-  var navBar = $('.navbar');
-navBar.append('<li class="marker"></li>');
+  var nav = $('.navbar');
 
-navBar.on("click" , "a" , function(){
-
-    var navTap = $(this).closest('.navbar');
+  nav.append('<li class="marker"></li>');
+  nav.on("mouseover" , "a" , function () {
+    var tap = $(this).closest('.navbar');
     var mrkWidth = $(this).parent('li').width();
-    var marker = navTap.find('.marker');
+    var marker = tap.find('.marker');
     var nx = $(".navbar").offset();
     var lx = $(this).parent('li').offset();
     var left = lx.left - nx.left;
+    nav.find('li').removeClass('active');
 
-   navBar.find('li').removeClass('active');
+    $(this).parent().addClass('active');
 
-   $(this).parent().addClass('active');
+    marker.removeClass("anim").css({ "width" : mrkWidth, "left" : left });
+    setTimeout(function () {
+      marker.addClass("anim");
+    }, 200);
+  });
 
-    marker.removeClass("anim").css({ "width" : mrkWidth , "left" : left });
-    setTimeout(function(){
-      marker.addClass("anim")
-    },200);
-});
+
+
+  var height = $(window).scrollTop();
+
+  $(window).on('mousewheel', function(e) {
+    height = $(window).scrollTop();
+    if (height > 700) {
+
+    }
+  });
+
+
+
 
 
   for(var i = 0; i < 9; i++) {
-    code += `        <!--panel -->
-            <div class=" col-sm-4">
-              <!-- Normal Demo-->
-                <!-- Post-->
-                <div class="post-module">
-                  <!-- Thumbnail-->
-                  <div class="thumbnail">
-                    <div class="date">
-                      <div class="day">` + (i + 1) + `</div>
-                    </div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg"/>
-                  </div>
-                  <!-- Post Content-->
-                  <div class="post-content">
-                    <div class="category">Photos</div>
-                    <h1 class="title">City Lights in New York</h1>
-                    <h2 class="sub_title">The city that never sleeps.</h2>
-                    <p class="description">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
-                    <div class="post-meta">
-                      <span class="timestamp"><i class="fa fa-clock-">o</i> 6 mins ago</span><span class="comments"><i class="fa fa-comments"></i><a href="#"> 39 comments</a></span>
-                    </div>
-                  </div>
+    code += `
+    <div class=" col-sm-4">
+      <div class="post-module">
+        <div class="thumbnail">
+          <div class="date">` + (i + 1) + `</div>
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg"/>
+        </div>
+        <div class="post-content">
+          <button class="category">More</button>
+          <h1 class="title">City Lights in New York</h1>
+          <h2 class="sub_title">Bigpictureteam project</h2>
+            <div class="post-meta">
+              <span class="timestamp"><i class="fa fa-clock-">*</i> 6 days</span><span class="comments"><i class="fa fa-comments">* </i><a href="#">5 people</a></span>
             </div>
-          </div>`;
+        </div>
+      </div>
+    </div>`;
   }
   $('.here').html(code);
+
+
+  backgroundEvent();
+
 });
